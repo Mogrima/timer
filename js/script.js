2577d109788f698e068f5f8ee8c922aec51c6a2d
Mogrima <rebel.rs@yandex.ru>
2019-03-27 15:19:24 +0500

    добавила в скрипт условие, чтобы к цифрам подставлялся ноль, если они меньше 10 и чтобы по окончании таймера просто высвечивались нули

--- js/script.js
+++ js/script.js
@@ -32,14 +32,35 @@ window.addEventListener('DOMContentLoaded', function () {
 
         function updateClock() {
             let t = getTimeRemaining(endtime);
-            days.textContent = t.days;
-            hours.textContent = t.hours;
-            minutes.textContent = t.minutes;
-            seconds.textContent = t.seconds;
+            
+            if (t.days < 10) {
+                days.textContent = '0' + t.days;
+            } else {
+                days.textContent = t.days;
+            }
+            if (t.hours < 10) {
+                hours.textContent = '0' + t.hours;
+            } else {
+                hours.textContent = t.hours;
+            }
+            if (t.minutes < 10) {
+                minutes.textContent = '0' + t.minutes;
+            } else {
+                minutes.textContent = t.minutes;
+            }
+            if (t.seconds < 10) {
+                seconds.textContent = '0' + t.seconds;
+            } else {
+                seconds.textContent = t.seconds;
+            }
             
 
             if (t.total <= 0) {
                 clearInterval(timeInterval);
+                days.textContent = '00';
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
             }
         }
     }
